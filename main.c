ea4d654664f1065473c50483b2d099a9738e1f22
Hammed Tosin <hammedtosin188@gmail.com>
2022-06-13 14:24:45 -0700

    betty

--- main.c
+++ main.c
@@ -11,7 +11,6 @@ int main(int argc, char *argv[])
 	size_t size = SIZE;
 	char *string;
 	char *token;
-	/* handles numbers of arguments*/
 	if (argc >= 2)
 	{
 	if (execve(argv[1], argv, NULL) == -1)
@@ -21,32 +20,21 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 	}
-	/* allocate a memory space for the buffer- string*/
 	string = malloc(size);
 	while (1)
 	{
 	printf("#cisfun$ ");
-
-	/* getline input from stdin and save in buffer */
 	if (getline(&string, &size, stdin) == EOF)
-		break;
-
-	/* get rid of trailing spaces and newline by adding NULL */
+	exit(0);
 	if (string[strlen(string) - 1] == '\n')
 	string[strlen(string) - 1] = '\0';
-
-	/* exit if "exit" is typed */
 	if (strncmp(string, "exit", 4) == 0)
 	break;
-	/* get string token if seperated by space */
 	token =	strtok(string, "\n");
 	while (token)
 	token = strtok(NULL, "\n");
-	
-	/* call process fun*/
 	process(&string);
 	}
-	/* free memory */
 	free(string);
 	return (0);
 }
